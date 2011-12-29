c7eb13bcba270d9c48e66a0ea17f1f1088164367
Aaron Cammarata <aaron@ajcgames.com>
2011-12-28 21:58:21 -0800

    Setting up starfield CSS anim started basic transition from main menu to performance test CSS and folder structure in place

---
+++ PGPT/www/javascripts/starfieldCSS.js
@@ -0,0 +1,6 @@
+function InitStarfieldCSS () {
+	var theMainMenu;
+	theMainMenu = document.getElementById("mainMenuContainer");
+	theMainMenu.parentNode.removeChild(theMainMenu);
+	alert("You are in the starfield, CSS'ing it!");
+}
