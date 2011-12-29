218113706d7f7a99c99fa7f486d34d974e1af456
Aaron Cammarata <aaron@ajcgames.com>
2011-12-29 10:54:34 -0800

    Basic moving starfield, using CSS transition

--- PGPT/www/javascripts/starfieldCSS.js
+++ PGPT/www/javascripts/starfieldCSS.js
@@ -1,6 +1,20 @@
-function InitStarfieldCSS () {
-	var theMainMenu;
+function InitStarfieldCSS() {
+	var theMainMenu, starfield, body, frame;
 	theMainMenu = document.getElementById("mainMenuContainer");
 	theMainMenu.parentNode.removeChild(theMainMenu);
-	alert("You are in the starfield, CSS'ing it!");
+	body = document.getElementsByTagName("body")[0];
+
+	/* Create the background starfield */
+	/* First create a container that will do the clipping for us via overflow:hidden */
+	frame = document.createElement("div");
+	frame.className = "frameContainer";
+	body.appendChild(frame);
+
+	/* Create a starfield background, and get it moving via a CSS Transform */
+	starfield = document.createElement("img");
+	starfield.className = "movingStarfield";
+	starfield.src = "media/images/starfield-actual.jpg";
+	frame.appendChild(starfield);
+	
+	
 }
