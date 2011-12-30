290d439242f3695a8f244dd00ef9163d0de70a43
Aaron Cammarata <aaron@ajcgames.com>
2011-12-29 18:07:06 -0800

    removing xcode-specific stuff

--- PGPT/www/tests/basicStarfield.js
+++ PGPT/www/tests/basicStarfield.js
@@ -1,4 +1,4 @@
-var debugOn, modeButton, moveMode, framerateDisplay, framerateInterval, framerateLow, framerateHigh, framerateAvg, lastTick, tickDuration, framerateDisplayUpdateFrequency, framerateDisplayUpdateCounter, thisTick, frameDuration, currentFramerate;
+var debugOn, body, testFrame, starfield, modeButton, moveMode, framerateDisplay, framerateInterval, framerateLow, framerateHigh, framerateAvg, lastTick, tickDuration, framerateDisplayUpdateFrequency, framerateDisplayUpdateCounter, thisTick, frameDuration, currentFramerate;
 
 /* Updates the framerate display - is called every tickDuration milliseconds via setInterval */
 function updateFramerateDisplay() {
@@ -35,42 +35,37 @@ function stopFramerateDisplayUpdate() {
 	debugOn = false;
 }
 
-/* Entry point. */
-function InitStarfieldCSS() {
-	var theMainMenu, starfield, body, frame;
-	theMainMenu = document.getElementById("mainMenuContainer");
-	theMainMenu.parentNode.removeChild(theMainMenu);
-	body = document.getElementsByTagName("body")[0];
-
+/* Set up the starfield using CSS. */
+function initStarfieldCSS() {
+	console.log("Initting CSS");
 	/* Create the background starfield */
 	/* First create a container that will do the clipping for us via overflow:hidden */
-	frame = document.createElement("div");
-	frame.className = "frameContainer";
-	body.appendChild(frame);
+	testFrame = document.createElement("div");
+	testFrame.className = "frameContainer";
+	body.appendChild(testFrame);
 
 	/* Add a framerate display */
 	framerateDisplay = document.createElement("div");
 	framerateDisplay.className = "framerateDisplay";
 	framerateDisplay.style.zIndex = 10;
 	framerateDisplay.innerHTML = "<center>Stand by<br>for<br>framerate... ";
-	frame.appendChild(framerateDisplay);
+	testFrame.appendChild(framerateDisplay);
 
 	/* Add a button to toggle CSS / JS movement */
 	modeButton = document.createElement("div");
 	modeButton.className = "simpleToggleButton";
 	modeButton.style.marginLeft = "300px";
 	modeButton.style.marginTop = "20px";
-	moveMode = "CSS";
 	modeButton.innerHTML = "Move mode: " + moveMode;
-
-	frame.appendChild(modeButton);
+	modeButton.onclick = toggleMoveMode;
+	testFrame.appendChild(modeButton);
 
 	/* Create a starfield background, and get it moving via a CSS Transform */
 	starfield = document.createElement("img");
 	starfield.style.zIndex = -10;
 	starfield.className = "movingStarfield";
 	starfield.src = "media/images/starfield-actual.jpg";
-	frame.appendChild(starfield);
+	testFrame.appendChild(starfield);
 
 	/* Turn on the framerate counter */
 	/* TickDuration holds the # of milliseconds before we will attempt to check framerate again.
@@ -92,4 +87,105 @@ function InitStarfieldCSS() {
 	framerateDisplayUpdateCounter = 0;
 	framerateDisplayUpdateFrequency = 100;
 	framerateInterval = setInterval(updateFramerate, tickDuration);
+}
+
+/* Set up the starfield using JavaScript. */
+function initStarfieldJS() {
+	console.log("Initting JS");
+	/* Create the background starfield */
+	/* First create a container that will do the clipping for us via overflow:hidden */
+	testFrame = document.createElement("div");
+	testFrame.className = "frameContainer";
+	body.appendChild(testFrame);
+
+	/* Add a framerate display */
+	framerateDisplay = document.createElement("div");
+	framerateDisplay.className = "framerateDisplay";
+	framerateDisplay.style.zIndex = 10;
+	framerateDisplay.innerHTML = "<center>Stand by<br>for<br>framerate... ";
+	testFrame.appendChild(framerateDisplay);
+
+	/* Add a button to toggle CSS / JS movement */
+	modeButton = document.createElement("div");
+	modeButton.className = "simpleToggleButton";
+	modeButton.style.marginLeft = "300px";
+	modeButton.style.marginTop = "20px";
+	modeButton.innerHTML = "Move mode: " + moveMode;
+	modeButton.onclick = toggleMoveMode;
+	testFrame.appendChild(modeButton);
+
+	/* Create a starfield background, and get it moving via a CSS Transform */
+	starfield = document.createElement("img");
+	starfield.style.zIndex = -10;
+	starfield.className = "movingStarfield";
+	starfield.src = "media/images/starfield-actual.jpg";
+	testFrame.appendChild(starfield);
+
+	/* Turn on the framerate counter */
+	/* TickDuration holds the # of milliseconds before we will attempt to check framerate again.
+	 * I'm using 1 for now, so that we get the best possible reading.
+	 */
+	tickDuration = 1;
+	/* Hack - I'm concerned that if we start measuring the max/min framerate right away, we will get
+	 * spurious readings during initialization. So I'm going to re-set the high and low once we're underway,
+	 * (1 second in) which should give us a better reading of high/lows during execution only.
+	 */
+	setTimeout(function() {
+		framerateHigh = 0;
+		framerateLow = 100000;
+		startFramerateDisplayUpdate();
+	}, 3000);
+	/* Counter is a temp variable. We will only update the framerate display every
+	 * framerateDisplayUpdateFrequency ticks of the framerate checking function.
+	 */
+	framerateDisplayUpdateCounter = 0;
+	framerateDisplayUpdateFrequency = 100;
+	framerateInterval = setInterval(updateFramerate, tickDuration);
+}
+
+/* Ditch the old one before we set up the next test */
+function clearStarfield() {
+	testFrame.parentNode.removeChild(testFrame);
+}
+
+function toggleMoveMode() {
+	console.log("Tapped on mode in mode " + moveMode);
+	switch (moveMode) {
+		case "CSS":
+			moveMode = "JS";
+			clearStarfield();
+			initStarfieldJS();
+			break;
+		case "JS":
+			moveMode = "CSS";
+			clearStarfield();
+			initStarfieldCSS();
+			break;
+	}
+}
+
+/* Entry point. */
+function initBasicStarfield() {
+	var theMainMenu;
+	theMainMenu = document.getElementById("mainMenuContainer");
+	theMainMenu.parentNode.removeChild(theMainMenu);
+	body = document.getElementsByTagName("body")[0];
+
+	/* Turn on the framerate counter */
+
+	/* TickDuration holds the # of milliseconds before we will attempt to check framerate again.
+	 * I'm using 1 for now, so that we get the best possible reading.
+	 */
+	tickDuration = 1;
+
+	/* Counter is a temp variable. We will only update the framerate display every
+	 * framerateDisplayUpdateFrequency ticks of the framerate checking function.
+	 */
+	framerateDisplayUpdateCounter = 0;
+	framerateDisplayUpdateFrequency = 100;
+	framerateInterval = setInterval(updateFramerate, tickDuration);
+	
+	/* Start in CSS mode */
+	moveMode = "CSS"
+	initStarfieldCSS();
 }
